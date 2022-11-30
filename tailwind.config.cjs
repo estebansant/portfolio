220803de541411a25b7e8ef72fd0f3b58bb42338
estebansant <estebansant30@gmail.com>
2022-11-30 12:22:00 -0300

    setting colors on flexbox

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      // Build your palette here
+      black: '#252422',
+      secondary_black: '#403d39',
+      gray: '#ccc5b9',
+      white: '#FAFAFA',
+      secondary_white: '#f1faee',
+      green: '#38b000',
+    },
     extend: {},
   },
   plugins: [],
