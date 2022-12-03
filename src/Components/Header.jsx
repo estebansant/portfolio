05de5cbb43461faa94f687d4f2b0614bf19aac20
estebansant <estebansant30@gmail.com>
2022-12-03 12:59:09 -0300

    added Esteban Santiago's logos

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import esteban from '../assets/icons/Esteban_Santiago.svg'
 
 const Header = () => {
   return (
     <header id="header" className="w-screen h-12 bg-gray">
-        <section className="w-screen flex items-center justify-center">
-            <div>
-                <p className="font-Merriweather text-md font-bold text-black">Esteban Santiago</p>
+        <section className="w-screen h-full flex items-center justify-center">
+            <div className="w-40 h-8/9 mt-1">
+                <img className="w-full h-full" src={esteban} alt="Esteban Santiago"/>
             </div>
         </section>
     </header>
