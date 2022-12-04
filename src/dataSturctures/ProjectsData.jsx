81418795d9404a6cee19d0242c7193a89e3af65e
estebansant <estebansant30@gmail.com>
2022-12-04 17:32:30 -0300

    created data structures for the project

---
+++ src/dataSturctures/ProjectsData.jsx
@@ -0,0 +1,73 @@
+import achorded from '../assets/images/projects/Achorded_screenshot.png';
+import istmo from '../assets/images/projects/Istmo_screenshot.png';
+import theScientificRoom from '../assets/images/projects/TSR_screenshot.png';
+import estebansant from '../assets/images/projects/Notfinal_screenshot.png';
+import todos from '../assets/images/projects/Todos_screenshot.png';
+import codes from '../assets/images/projects/Codes_screenshot.png';
+import batatabit from '../assets/images/projects/Batatabit_screenshot.png';
+
+export const ProjectsData = [
+    {
+        image: achorded,
+        alt: 'achorded preview',
+        title: 'Achorded',
+        skills: ['React', 'SCSS', 'JavaScript', 'Webpack'],
+        description: 'I used React, and some libraries like React Router and Framer Motion, to create a working e-commerce about electric and acoustic guitars.',
+        link: 'https://achorded.netlify.app/',
+        code: 'https://github.com/estebansant/achorded',
+    },
+    {
+        image: istmo,
+        alt: 'istmo preview',
+        title: 'Istmo Consultores',
+        skills: ['HTML', 'CSS', 'SCSS', 'JavaScript'],
+        description: 'I made this website for Istmo Consultores, A consulting agency in Argentina. I mostly used HTML, SCSS and JS to develop the whole website.',
+        link: 'http://www.istmo.com.ar/',
+        code: 'https://github.com/estebansant/istmo',
+    },
+    {
+        image: theScientificRoom,
+        alt: 'the scientific room preview',
+        title: 'The Scientific Room',
+        skills: ['HTML', 'CSS', 'SCSS', 'JavaScript'],
+        description: 'This was my first personal project, where I showcase science videos in English and in Spanish grouped in multiple categories.',
+        link: 'https://estebansant.github.io/The_Scientific_Room/',
+        code: 'https://github.com/estebansant/The_Scientific_Room',
+    },
+    {
+        image: estebansant,
+        alt: 'estebansant preview',
+        title: 'Estebansant.com',
+        skills: ['React', 'Tailwind', 'JavaScript', 'Vite'],
+        description: 'This is my personal portfolio website. Built with React, Tailwind and JavaScript. Not much else to say because you are looking at it.',
+        link: 'https://www.estebansant.com',
+        code: 'https://github.com/estebansant/portfolio',
+    },
+    {
+        image: todos,
+        alt: 'todos app preview',
+        title: 'Todo List',
+        skills: ['React', 'CSS', 'JavaScript'],
+        description: 'This was my first React project. It is a Todos app where you can create, complete, search for todos you have created, and also delte them.',
+        link: 'https://estebansant.github.io/todos_app/',
+        code: 'https://github.com/estebansant/todos_app',
+    },
+    {
+        image: codes,
+        alt: 'security codes preview',
+        title: 'Security Codes',
+        skills: ['React', 'JavaScript'],
+        description: 'Simple React project to test and practice with states, reducers and classes. The secret word is "paradigma". Use it and watch how it works',
+        link: 'https://estebansant.github.io/security_codes/',
+        code: 'https://github.com/estebansant/security_codes',
+    },
+    {
+        image: batatabit,
+        alt: 'batatabit preview',
+        title: 'Batatabit',
+        skills: ['HTML', 'SCSS'],
+        description: 'This was my first project using HTML and CSS, where I mostly practiced the mobile first design. It is the very beginning of my path to learn code.',
+        link: 'https://estebansant.github.io/Batabit-project/',
+        code: 'https://github.com/estebansant/Batabit-project',
+    },
+]
---
+++ src/dataSturctures/SkillsData.jsx
@@ -0,0 +1,51 @@
+import html from '../assets/images/logos/HTML_logo.png';
+import css from '../assets/images/logos/CSS_logo.png';
+import js from '../assets/images/logos/JS_logo.png';
+import reactLogo from '../assets/images/logos/React_logo.png';
+import git from '../assets/images/logos/Git_logo.png';
+import tailwind from '../assets/images/logos/Tailwind_logo.png';
+import sass from '../assets/images/logos/Sass_logo.png';
+import vite from '../assets/images/logos/Vite_logo.png';
+
+export const SkillsData = [
+    {
+        image: html,
+        alt: 'html logo',
+        title: 'HTML',
+    },
+    {
+        image: css,
+        alt: 'css logo',
+        title: 'CSS',
+    },
+    {
+        image: js,
+        alt: 'javascript logo',
+        title: 'JavaScript',
+    },
+    {
+        image: reactLogo,
+        alt: 'react logo',
+        title: 'React',
+    },
+    {
+        image: git,
+        alt: 'gitl logo',
+        title: 'Git and GitHub',
+    },
+    {
+        image: tailwind,
+        alt: 'tailwind logo',
+        title: 'Tailwind',
+    },
+    {
+        image: sass,
+        alt: 'sass scss logo',
+        title: 'SCSS',
+    },
+    {
+        image: vite,
+        alt: 'vite logo',
+        title: 'Vite',
+    },
+]
