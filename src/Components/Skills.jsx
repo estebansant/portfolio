f2b8ff57822c297e280cb2a6ece2eb3a7c7757aa
estebansant <estebansant30@gmail.com>
2022-12-05 00:00:21 -0300

    completed skills mobile design

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { SkillsData } from '../dataStructures/SkillsData'
 
 const Skills = () => {
   return (
-    <section id="skills" className="w-full h-auto">
-      <h2>Skills</h2>
-      <div>
-        <div>
-          <div>
-            <img />
-          </div>
-          <div>
-            <p></p>
-          </div>
-        </div>
+    <section id="skills" className="w-full h-auto mt-6 px-6">
+      <h2 className="font-Merriweather text-black font-bold text-2xl">Skills</h2>
+      <div className="w-auto h-64 flex items-center overflow-x-auto overscroll-x-contain scroll-smooth mr-4">
+
+        {SkillsData.map((skill, index) => {
+          return(
+            <div key={index} className="w-3/5 h-4/5 bg-secondary-white flex flex-none flex-col justify-center items-center p-7 rounded-lg shadow-md mr-3 ml-3 transition transition-all duration-300 ease-in-out hover:bg-secondary-black text-secondary-black hover:text-secondary-white">
+              <div className="w-5/5 h-4/5">
+                <img className="w-full h-full" src={skill.image} alt={skill.alt}/>
+              </div>
+              <div className="w-full h-auto text-center mt-4">
+                <p className="font-Merriweather font-bold text-xl">{skill.title}</p>
+              </div>
+            </div>
+          )
+        })}
+
       </div>
     </section>
   )
