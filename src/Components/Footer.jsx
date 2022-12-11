54b1ada92c1d17464ea30d876e36907a2cdcd31e
estebansant <estebansant30@gmail.com>
2022-12-11 11:21:11 -0300

    finished footer styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import twitter from '../assets/icons/twitter_logo.svg';
+import github from '../assets/icons/github.svg';
+import linkedin from '../assets/icons/linkedin_logo.svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="p-5 w-full h-auto flex items-center justify-around bg-secondary-white mt-10 gap-2">
+      <div className="flex flex-col space-y-2 items-center justify-center">
+        <p className="text-base font-Lato font-semibold text-secondary-black">Social media</p>
+        <div className="flex flex-row items-center justify-around gap-3">
+          <a
+            href="https://github.com/estebansant"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-8 h-8"
+          >
+            <img src={github} className="w-8 h-8 bg-cover"/>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/esteban-s-25b76113a/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-8 h-8"
+            >
+            <img src={linkedin} className="w-8 h-8 bg-cover"/>
+          </a>
+          <a
+            href="https://twitter.com/esteban_sant30"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-8 h-8"
+          >
+            <img src={twitter} className="w-8 h-8 bg-cover"/>
+          </a>
+        </div>
+      </div>
+      <div>
+        <a href="" className="hover:underline">
+          <p className="text-base font-Lato font-semibold text-secondary-black">Download CV</p>
+        </a>
+      </div>
+    </footer>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,11 +4,11 @@ import esteban from '../assets/icons/Esteban_Santiago.svg'
 const Header = () => {
   return (
     <header id="header" className="w-screen h-12 bg-secondary-white fixed top-0 z-20 shadow-md2">
-        <section className="w-screen h-full flex items-center justify-center">
+        <div className="w-screen h-full flex items-center justify-center">
             <a href="#home" className="w-40 h-8/9 mt-1">
                 <img className="w-full h-full" src={esteban} alt="Esteban Santiago"/>
             </a>
-        </section>
+        </div>
     </header>
   )
 }
