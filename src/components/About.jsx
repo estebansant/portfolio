3be04f0fba79b8f7799dd71bf3b87a8eed4178c6
estebansant <estebansant30@gmail.com>
2022-12-15 15:47:58 -0300

    finished mobile version

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id="about" className="w-full h-auto">
       <h2 className="w-full mt-5 z-10 text-center relative text-3xl font-Merriweather font-bold before:h-0.5 before:w-1/2 before:bg-black before:absolute before:top-1/2 before:right-0 before:-z-10 after:h-0.5 after:w-1/2 after:bg-black after:absolute after:top-1/2 after:left-0 after:-z-10"><span className="w-80 bg-white z-10 py-0 px-5">About me</span></h2>
-      <p className="w-full px-6 mt-4 mb-4 text-justify text-base font-Lato font-normal">I am a young physics student who learned all that he knows about coding thanks to <a href="https://platzi.com/" target="_blank" rel="noopener noreferrer" className="underline">Platzi</a>, <a href="https://stackoverflow.com/" target="_blank" rel="noopener noreferrer" className="underline">StackOverflow</a> and many hours of reading through documentation files. <br/><br/> I am currently looking for a job as a frontend developer. Contact me to start working together.</p>
+      <p className="w-full px-6 mt-4 mb-4 text-justify text-base font-Lato font-normal">I am a young physics student who learned all that he knows about coding thanks to <a href="https://platzi.com/" target="_blank" rel="noopener noreferrer" className="underline">Platzi</a>, <a href="https://stackoverflow.com/" target="_blank" rel="noopener noreferrer" className="underline">StackOverflow</a> and many hours of reading through documentation files. So, basically I am a self taught programmer. <br/><br/> I am currently looking for a job as a frontend developer. Contact me to start working together.</p>
     </section>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from 'react';
+import backgroundMobile from '../assets/images/background_small.jpg';
+import background from '../assets/images/background_big.jpg';
+import desktop from '../assets/images/desktop.png';
 
 const Home = () => {
   return (
-    <section id="home" className="w-full h-screen flex flex-col justify-around">
-      <div className="w-full">
+    <section id="home" className="w-full h-screen flex flex-col justify-around relative overflow-hidden">
+
+      <picture className="w-full h-full z-0 absolute top-0">
+          <source media="(min-width:768px)" srcSet={background} className="w-full h-full"/>
+          <img src={backgroundMobile} alt="music festival" className="w-full h-full"/>
+      </picture>
+
+      <img src={desktop} alt="desktop with computer" className="w-[85%] h-auto absolute bottom-[20%] -right-[10%] z-10 transition transition-all duration-300 ease-in-out hover:scale-110"/>
+
+      <div className="w-full z-10">
         <h1 className="pt-6 px-6 text-3xl font-Merriweather font-bold text-black">Hi! I'm Esteban Santiago</h1>
         <h2 className="w-4/5 mt-2 px-6 text-xl font-Lato font-bold text-secondary-black leading-none">Frontend developer in React</h2>
       </div>
       
-      <div className="w-full h-auto px-4 flex justify-center items-center shrink-0 space-x-3">
+      <div className="w-full h-auto px-4 flex justify-center items-center shrink-0 space-x-3 z-10">
         <a href="#contact" className="w-36 h-12 bg-black rounded-md flex items-center justify-center shadow-md transtition transition-all duration-300 easi-in-out hover:bg-secondary-white">
           <p className="px-5 py-1 text-white font-Lato font-semibold text-lg transtition transition-all duration-300 easi-in-out hover:text-black">Contact me</p>
         </a>
